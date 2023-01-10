161a3a660523a4614d2d8c135687d5d050bb0b5d
Anastasiia Smilka <anastasiia.smilka@globallogic.com>
2023-01-10 18:27:12 +0200

    add fragment to map

--- app/src/main/java/com/example/weatherapp/MapsActivity.java
+++ app/src/main/java/com/example/weatherapp/MapsActivity.java
@@ -1,33 +1,29 @@
 package com.example.weatherapp;
 
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
-import com.example.weatherapp.model.MainModel;
+import com.example.weatherapp.databinding.ActivityMapsBinding;
 import com.example.weatherapp.model.Response;
-import com.example.weatherapp.model.Weather;
-import com.example.weatherapp.model.Wind;
 import com.example.weatherapp.networking.WeatherAPI;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.weatherapp.databinding.ActivityMapsBinding;
 import com.google.gson.Gson;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Objects;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -39,7 +35,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(binding.getRoot());
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.map_fragment);
         mapFragment.getMapAsync(this);
     }
 
@@ -47,15 +43,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         googleMap.setOnMapClickListener(point -> {
             googleMap.clear();
-            Log.d("Map", "Map clicked" + point);
-            googleMap.addMarker(new MarkerOptions().position(point)
-                    .title(getCurrentWeather(point.latitude, point.longitude)));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 7));
+            googleMap.addMarker(new MarkerOptions().position(point));
+            getCurrentWeather(point.latitude, point.longitude);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 8));
         });
     }
 
-    private String getCurrentWeather(double lat, double lon) {
-        String[] weatherMessage = new String[1];
+    private void getCurrentWeather(double lat, double lon) {
         AndroidNetworking.get(WeatherAPI.BASEURL + WeatherAPI.CurrentWeather + "lat=" + lat + "&lon=" + lon + WeatherAPI.UnitsAppid)
                 .setPriority(Priority.MEDIUM)
                 .build()
@@ -66,16 +60,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         Response weatherResponse;
 
                         weatherResponse = gson.fromJson(response.toString(), Response.class);
-                        weatherMessage[0] = "It`s " + weatherResponse.getWeather().get(0).getDescription() + " and " + weatherResponse.getMain().getTemp() + "°C in " + weatherResponse.getName();
 
-                        Toast.makeText(getApplicationContext(), weatherMessage[0], Toast.LENGTH_SHORT).show();
+                        String description = "It`s " + weatherResponse.getWeather().get(0).getDescription()
+                                + " and " + weatherResponse.getMain().getTemp()
+                                + "°C in " + weatherResponse.getName();
+
+                        TextView textView = findViewById(R.id.weather_details_in_fragment);
+                        textView.setText(description);
                     }
 
                     @Override
                     public void onError(ANError anError) {
                     }
                 });
-
-        return weatherMessage[0];
     }
 }
